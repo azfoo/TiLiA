39b4e631da06ac01a150d9bde8582c7ff08f4057
azfoo <45888544+azfoo@users.noreply.github.com>
2025-03-03 20:04:05 +0000

    refactor: abstract musicxml reader

---
+++ tilia/parsers/musicxml/base.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+from zipfile import ZipFile
+from typing import Any, Optional
+
+from lxml import etree
+
+
+class TiliaMXLReader:
+    def __init__(
+        self,
+        path: str,
+        file_kwargs: Optional[dict[str, Any]] = None,
+        reader_kwargs: Optional[dict[str, Any]] = None,
+    ):
+        self.path = Path(path)
+        self.file_kwargs = file_kwargs or {}
+        self.reader_kwargs = reader_kwargs or {}
+
+    def _get_mxl_data(self):
+        with (
+            ZipFile(self.path) as zipfile,
+            zipfile.open("META-INF/container.xml", **self.file_kwargs) as meta,
+        ):
+            full_path = (
+                etree.parse(meta, **self.reader_kwargs)
+                .findall(".//rootfile")[0]
+                .get("full-path")
+            )
+            data = zipfile.open(full_path, **self.file_kwargs)
+        return data
+
+    def __enter__(self):
+
+        if ".mxl" == self.path.suffix:
+            self.file = self._get_mxl_data()
+        else:
+            self.file = open(self.path, **self.file_kwargs)
+
+        parser = etree.XMLParser(remove_blank_text=True)
+        self.tree = etree.parse(
+            self.file, parser=parser, **self.reader_kwargs
+        ).getroot()
+        self.is_read = True
+
+        if self.tree.tag == "score-timewise":
+            self.tree = _convert_to_partwise(self.tree)
+        elif self.tree.tag != "score-partwise":
+            self.is_read = False
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.file.close()
+
+
+def _convert_to_partwise(element: etree._Element) -> etree._Element:
+    xsl_path = Path("parsers", "musicxml", "timewise_to_partwise.xsl")
+    with open(str(xsl_path.resolve()), "r", encoding="utf-8") as xsl:
+        xsl_tree = etree.parse(xsl)
+
+    transform = etree.XSLT(xsl_tree)
+    return transform(element)
+
+
+def _pretty_str_from_xml_element(element: etree._Element):
+    return etree.tostring(element, pretty_print=True).decode("utf-8")
