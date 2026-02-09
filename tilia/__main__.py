6e410579f5a26a1c3a9f558c12fd107651eb4e7a
azfoo <45888544+azfoo@users.noreply.github.com>
2026-02-09 13:14:42 +0000

    .

--- tilia/__main__.py
+++ tilia/__main__.py
@@ -3,8 +3,13 @@ from pathlib import Path
 
 sys.path[0] = Path(__file__).parents[1].__str__()
 
-from tilia.boot import boot  # noqa: E402
-
-
 if __name__ == "__main__":
-    boot()
+    try:
+        from tilia.boot import boot  # noqa: E402
+
+        boot()
+    except ImportError as e:
+        import traceback
+
+        print([*traceback.walk_tb(e.__traceback__)][0][0].f_code.co_filename)
+        print(e.path)
