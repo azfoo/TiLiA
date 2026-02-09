233a3ab133162f42230dae8352f3946171e1b3dc
azfoo <45888544+azfoo@users.noreply.github.com>
2026-02-09 22:39:48 +0000

    .

--- tilia/__main__.py
+++ tilia/__main__.py
@@ -1,5 +1,7 @@
 import sys
 from pathlib import Path
+import subprocess
+import traceback
 
 sys.path[0] = Path(__file__).parents[1].__str__()
 
@@ -9,23 +11,20 @@ if __name__ == "__main__":
 
         boot()
     except ImportError as e:
-        import subprocess
-        import traceback
-
         root_path = Path(
             [*traceback.walk_tb(e.__traceback__)][0][0].f_code.co_filename
         ).parent
-        missing_deps = subprocess.Popen(
+        lib_path = root_path / "PySide6/qt-plugins/platforms/libqxcb.so"
+        missing_deps = subprocess.check_output(
             [
                 "ldd",
-                (root_path / "PySide6/qt-plugins/platforms/libqxcb.so").as_posix(),
+                lib_path.as_posix(),
                 "|",
                 "grep",
                 '"not found"',
             ],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-        )
-        for line in missing_deps.stdout.readlines():
+            shell=True,
+        ).decode()
+        for line in missing_deps.splitlines():
             dep = line.strip().rstrip(" => not found")
-            print(dep)
+            print(line, dep, sep="\t")
