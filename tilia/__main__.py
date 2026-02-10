6493f7a09b6fc5678fcd51cf15760621b9edc5bd
azfoo <45888544+azfoo@users.noreply.github.com>
2026-02-10 09:36:30 +0000

    .

--- tilia/__main__.py
+++ tilia/__main__.py
@@ -15,16 +15,12 @@ if __name__ == "__main__":
             [*traceback.walk_tb(e.__traceback__)][0][0].f_code.co_filename
         ).parent
         lib_path = root_path / "PySide6/qt-plugins/platforms/libqxcb.so"
-        missing_deps = subprocess.check_output(
-            [
-                "ldd",
-                lib_path.as_posix(),
-                "|",
-                "grep",
-                '"not found"',
-            ],
-            shell=True,
+        deps = subprocess.check_output(
+            ["ldd", lib_path.as_posix()], shell=True
         ).decode()
-        for line in missing_deps.splitlines():
-            dep = line.strip().rstrip(" => not found")
-            print(line, dep, sep="\t")
+        for line in deps.splitlines():
+            if "=> not found" in line:
+                dep = line.strip().rstrip(" => not found")
+                print("\t", dep)
+            else:
+                print(line)
